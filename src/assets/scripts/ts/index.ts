21da06974feb3ca64f860b4edd4eb89e526aa43e
Aleksandr Mironenko <alfie.mir@gmail.com>
2020-07-15 20:56:49 +0300

    Создал шаблон модального окна. Удалил лишÐние классы и Ñфайлы стилей и криптов. УбУБрбрал babel polyfill. Так Ðже Добавил свой polyfill для foreach

--- src/assets/scripts/ts/index.ts
+++ src/assets/scripts/ts/index.ts
@@ -9,152 +9,69 @@ document.addEventListener('DOMContentLoaded', () => {
     };
   }
 
-  class FiliModal {
-    buttons: NodeList;
-    modal: HTMLElement;
-    closeBtn: HTMLElement;
+  filiModal();
 
-    constructor() {
-      this.buttons = document.querySelectorAll(`[data-modal-button]`);
-      this.modal = document.querySelector(`[data-modal-show="true"]`);
-
-      if (this.modal) {
-        this.closeBtn = this.modal.querySelector('[data-modal-close="close"]');
-      }
-    }
-
-    init() {
-      if (this.modal) {
-        this.firstShow(this.modal);
-      }
-
-      this.buttons.forEach(button => button.addEventListener('click', this.showModal));
-    }
-
-    showModal = (e) => {
-      e.preventDefault();
-      const id = e.target.dataset.modalButton;
-      this.modal = document.querySelector(`[data-modal-id=${ id }]`);
-
-      this.modal.classList.remove('fili-modal--is-hidden');
-      this.modal.classList.add('fili-modal--is-visible');
-
-      this.close(this.modal);
-    };
-
-    closeModal = overlay => {
-      this.modal.classList.add('fili-modal--is-hidden');
-      this.modal.classList.remove('fili-modal--is-visible');
-
-      let timerId = setTimeout(() => {
-        this.modal.classList.remove('fili-modal--is-hidden');
-        clearTimeout(timerId);
-      }, 500);
-
-      this.destroyOverlay(overlay);
-    };
-
-    createOverlay = () => {
-      const overlay = document.createElement('div');
-      overlay.classList.add('fili-overlay');
-      document.body.appendChild(overlay);
-
-      return overlay;
-    };
-
-    destroyOverlay = (el: HTMLElement) => {
-      try {
-        const parent = el.parentNode;
-        parent.removeChild(el);
-      } catch (err) {
-        console.info(err);
-      }
-    };
-
-    firstShow = modal => {
-      modal.classList.remove('fili-modal--is-hidden');
-      modal.classList.add('fili-modal--is-visible');
-
-      this.close(modal);
-    };
-
-    close = modal => {
-      const closeButton = modal.querySelector('[data-modal-close="close"]');
-
-      const overlay = this.createOverlay();
-
-      overlay.addEventListener('click', e => this.closeModal(overlay));
-      closeButton.addEventListener('click', e => this.closeModal(overlay));
-    };
-  }
-
-  // const m = new FiliModal();
-  // m.init();
-
-  f();
-
-  function f() {
+  function filiModal() {
     const buttons = document.querySelectorAll(`[data-modal-button]`);
-    let modal = document.querySelector(`[data-modal-show="true"]`);
+    let modal = document.querySelector(`[data-modal-show="true"]`); // Если модалка должна открываться при загрузке
+    // страницы
+    let closeButton;
+    let overlay;
 
-    const showModal = (e) => {
-      e.preventDefault();
-      const id = e.target.dataset.modalButton;
-      modal = document.querySelector(`[data-modal-id=${ id }]`);
+    if (modal) {
+      init();
+    }
 
-      modal.classList.remove('fili-modal--is-hidden');
-      modal.classList.add('fili-modal--is-visible');
+    function openModal(button) {
+      button.addEventListener('click', function () {
+        const id = this.dataset.modalButton;
+        modal = document.querySelector(`[data-modal-id="${ id }"]`); // Переопределяем модалку
 
-      close(modal);
-    };
+        init();
+      });
+    }
 
-    const createOverlay = () => {
+    /**
+     * Создание overlay
+     */
+    function createOverlay() {
       const overlay = document.createElement('div');
       overlay.classList.add('fili-overlay');
       document.body.appendChild(overlay);
-
       return overlay;
-    };
+    }
 
-    const destroyOverlay = (el: HTMLElement) => {
-      try {
-        const parent = el.parentNode;
-        parent.removeChild(el);
-      } catch (err) {
-        console.info(err);
-      }
-    };
-
-    const closeModal = (modal, overlay) => {
+    function closeModal() {
       modal.classList.add('fili-modal--is-hidden');
       modal.classList.remove('fili-modal--is-visible');
 
-      let timerId = setTimeout(() => {
+      overlay.classList.remove('fili-overlay');
+      document.body.removeChild(overlay);
+
+      closeButton.removeEventListener('click', closeModal);
+      overlay.removeEventListener('click', closeModal);
+
+      const timerID = setTimeout(function () {
         modal.classList.remove('fili-modal--is-hidden');
-        clearTimeout(timerId);
+        clearTimeout(timerID);
       }, 500);
+    }
 
-      destroyOverlay(overlay);
-    };
+    function init() {
+      closeButton = modal.querySelector('[data-modal-close="close"]');
 
-    const close = modal => {
-      const closeButton = modal.querySelector('[data-modal-close="close"]');
-
-      const overlay = createOverlay();
-
-      overlay.addEventListener('click', e => closeModal(modal, overlay));
-      closeButton.addEventListener('click', e => closeModal(modal, overlay));
-    };
-
-    // Первый показ
-    if (modal) {
-      const closeBtn = modal.querySelector('[data-modal-close="close"]');
+      overlay = createOverlay(); // Переопределяем оверлей
 
       modal.classList.remove('fili-modal--is-hidden');
       modal.classList.add('fili-modal--is-visible');
-      close(modal);
+
+      closeButton.addEventListener('click', closeModal);
+      overlay.addEventListener('click', closeModal);
     }
 
-    buttons.forEach(button => button.addEventListener('click', showModal));
+    /**
+     * Запуск события клика на модальное окно
+     */
+    buttons.forEach(openModal);
   }
 });
