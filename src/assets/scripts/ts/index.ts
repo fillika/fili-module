ec47515e4f8656d1c29f3298e8ef3439fda7691d
Aleksandr Mironenko <alfie.mir@gmail.com>
2020-07-23 22:56:16 +0300

    Добавил файл для типов. Добавил TS init файл. Так же переделал поиск кнопки закрытия по селектору - теперь их может быть сколько угодно, главное повесить на любой элемент тот же дата-атрибут

--- src/assets/scripts/ts/index.ts
+++ src/assets/scripts/ts/index.ts
@@ -1,6 +1,6 @@
-document.addEventListener('DOMContentLoaded', () => {
-  if ('NodeList' in window && !NodeList.prototype.forEach) {
-    console.info('polyfill for IE11');
+document.addEventListener("DOMContentLoaded", () => {
+  if ("NodeList" in window && !NodeList.prototype.forEach) {
+    console.info("polyfill for IE11");
     NodeList.prototype.forEach = function (callback, thisArg) {
       thisArg = thisArg || window;
       for (let i = 0; i < this.length; i++) {
@@ -12,22 +12,21 @@ document.addEventListener('DOMContentLoaded', () => {
   filiModal();
 
   function filiModal() {
-    const buttons = document.querySelectorAll(`[data-modal-button]`);
+    const buttons: NodeList = document.querySelectorAll(`[data-modal-button]`);
     const overlay = createOverlay(); // Создаем overlay
 
     let modal = document.querySelector(`[data-modal-show="true"]`); // Если модалка должна открываться при загрузке
     // страницы
-    let closeButton;
+    let closeButton: NodeList;
 
     if (modal) {
       init();
     }
 
-    function openModal(button) {
-      button.addEventListener('click', function () {
+    function openModal(button: Node): void {
+      button.addEventListener("click", function (this: HTMLElement): void {
         const id = this.dataset.modalButton;
-
-        modal = document.querySelector(`[data-modal-id="${ id }"]`); // Переопределяем модалку
+        modal = document.querySelector(`[data-modal-id="${id}"]`); // Переопределяем модалку
 
         if (modal) {
           init();
@@ -39,41 +38,43 @@ document.addEventListener('DOMContentLoaded', () => {
      * Создание overlay
      */
     function createOverlay(): HTMLElement {
-      const overlay = document.createElement('div');
-      overlay.classList.add('fili-overlay', 'fili-overlay--is-hidden');
+      const overlay = document.createElement("div");
+      overlay.classList.add("fili-overlay", "fili-overlay--is-hidden");
       document.body.appendChild(overlay);
       return overlay;
     }
 
     function closeModal(): void {
-      modal.classList.add('fili-modal--is-hidden');
-      modal.classList.remove('fili-modal--is-visible');
+      modal?.classList.add("fili-modal--is-hidden");
+      modal?.classList.remove("fili-modal--is-visible");
 
-      if (closeButton) {
-        closeButton.removeEventListener('click', closeModal); // Удаляем обработчик
-      }
+      closeButton.forEach((btn: Node) =>
+        btn.removeEventListener("click", closeModal)
+      );
 
-      overlay.removeEventListener('click', closeModal); // Удаляем обработчик
-      overlay.classList.add('fili-overlay--is-hidden'); // Скрываем оверлей
+      overlay.removeEventListener("click", closeModal); // Удаляем обработчик
+      overlay.classList.add("fili-overlay--is-hidden"); // Скрываем оверлей
 
       const timerID = setTimeout(function () {
-        modal.classList.remove('fili-modal--is-hidden');
+        modal?.classList.remove("fili-modal--is-hidden");
         clearTimeout(timerID);
       }, 500);
     }
 
     function init(): void {
-      modal.classList.remove('fili-modal--is-hidden');
-      modal.classList.add('fili-modal--is-visible');
-      overlay.classList.remove('fili-overlay--is-hidden');
+      modal?.classList.remove("fili-modal--is-hidden");
+      modal?.classList.add("fili-modal--is-visible");
+      overlay.classList.remove("fili-overlay--is-hidden");
 
-      closeButton = modal.querySelector('[data-modal-close="close"]');
-
-      if (closeButton) {
-        closeButton.addEventListener('click', closeModal);
+      if (modal) {
+        closeButton = modal.querySelectorAll('[data-modal-close="close"]');
       }
 
-      overlay.addEventListener('click', closeModal);
+      closeButton.forEach((btn: Node) =>
+        btn.addEventListener("click", closeModal)
+      );
+
+      overlay.addEventListener("click", closeModal);
     }
 
     /**
